108b6a73b23e1dabeb10241345271008fd324a42
Sayo Melu <sayo-melu@outlook.com>
2019-07-26 10:53:48 +0800

    Remove !, [tab], [space] from filter.txt

--- Generate Filter.sh
+++ Generate Filter.sh
@@ -5,17 +5,32 @@ header="[Adblock Plus 3.0]
 ! Homepage: https://github.com/sayo-melu/nothingblock
 ! Licence: https://github.com/sayo-melu/nothingblock/blob/master/LICENSE
 ! Expires: 2 days
-! Version: $(date +%Y.%m.%d)
-!"
+! Version: $(date +%Y.%m.%d)"
 
 rm filter.txt
 touch filter.txt
 
 echo  "$header" >> filter.txt
 
-for item in filter-item/*
+for item in Item/*
 do
-	 cat $item >> filter.txt
+	cat $item >> filter.txt
 done
+
+case $(uname) in
+
+  Linux)
+    sed -i 's/^ *//g' filter.txt
+    sed -i 's/^\t*//g' filter.txt
+    sed -i '7,${/^!/d}' filter.txt
+  ;;
+
+  Darwin)
+    gsed -i 's/^ *//g' filter.txt
+	  gsed -i 's/^\t*//g' filter.txt
+	  gsed -i '7,${/^!/d}' filter.txt || echo -e '\nError, macOS should install gnu-sed.\n'; exit
+  ;;
+
+esac
 
 echo 'Filter generated.'
